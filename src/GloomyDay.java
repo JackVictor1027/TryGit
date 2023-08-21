4daec203f54c417bfb1254abb8dc06e4daccf0ec
Jack <jackvictor@163.com>
2023-08-21 15:08:53 +0800

    hot-fix 1st commit

--- src/GloomyDay.java
+++ src/GloomyDay.java
@@ -2,5 +2,8 @@ public class GloomyDay {
     public void Test_Response(){
         System.out.println("Great Days!");
         System.out.println("Let's voice of love take you higher!");
+        System.out.println("Golden Experience!");
+
+        System.out.println("hello git!")
     }
 }
